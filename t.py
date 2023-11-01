07687aea9d57e6da0160aca157e9644b361a9101
wanghaisheng <edwin.uestc@gmail.com>
2023-11-01 17:22:52 +0800

    country db

--- t.py
+++ t.py
@@ -2,7 +2,7 @@ import xmltodict
 import jsons
 def load_json(xml_path):
     #获取xml文件
-    xml_file = open(xml_path, 'r')
+    xml_file = open(xml_path, 'r',encoding='utf-8')
     #读取xml文件内容
     xml_str = xml_file.read()
     #将读取的xml内容转为json
@@ -10,6 +10,11 @@ def load_json(xml_path):
     return json
 filename=r'D:\Download\audio-visual\saas\tiktoka\tiktoka-studio-uploader-genius\assets\country-db\qq\LocList.xml'
 r=load_json(filename)
-with open('1.json','w') as f:
-        f.write(jsons.dumps(r))      
-print(r['Location']['CountryRegion'])
+coutr=[ x['@Name'] for x in r['Location']['CountryRegion']]
+for c in r['Location']['CountryRegion']:
+    if c['@Name']=='美国':
+        states=[x['@Name'] for x in c['State']]
+        print(states)
+# print(coutr)
+# with open('1.json','w') as f:
+#         f.write(jsons.dumps(r))      
