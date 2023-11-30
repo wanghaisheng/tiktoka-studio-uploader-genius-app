318ac383f3a214264ef5aac4a4b3c9e6194f6388
wanghaisheng <edwin_uestc@163.com>
2023-11-30 15:52:04 +0800

    fix ci

---
+++ docs/release.md
@@ -0,0 +1,2 @@
+https://micahflee.com/2023/10/what-goes-into-making-an-onionshare-release-part-3/#notarizing-the-app
+
--- setup.py
+++ setup.py
@@ -1,41 +1,111 @@
+# https://github.com/Futura-Py/TimerX/tree/master
 import sys
-from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need fine tuning.
+from cx_Freeze import Executable, setup
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+    icon = "./assets/icon.ico"
+    executables = [
+        Executable(
+            "uploadergenius",
+            base=base,
+            icon=icon,
+            shortcut_name="UploaderGenius",
+            target_name="UploaderGenius.exe",
+            shortcut_dir="ProgramMenuFolder",
+        )
+    ]
+elif sys.platform == "darwin":
+
+    # icon = "./assets/logo_new.icns"
+    icon = "./assets/icon.ico"
+
+    executables = [
+        Executable(
+            "uploadergenius",
+            base=base,
+            icon=icon,
+            shortcut_name="UploaderGenius",
+            target_name="UploaderGenius.exe",
+        )
+    ]
+else:
+    # icon = "./assets/logo_new.png"
+    icon = "./assets/icon.ico"
+
+    executables = [
+        Executable(
+            "uploadergenius",
+            base=base,
+            icon=icon,
+            shortcut_name="UploaderGenius",
+            target_name="UploaderGenius.exe",
+        )
+    ]
+
+# directory_table = [
+#     ("ProgramMenuFolder", "TARGETDIR", "."),
+#     ("MyProgramMenu", "ProgramMenuFolder", "MYPROG~1|My Program"),
+# ]
+
+# msi_data = {
+#     "Directory": directory_table,
+#     "ProgId": [
+#         ("UploaderGenius", None, None, "A simple, lightweight, & beautiful timer app built in Python and tkinter.ttk using rdbende's Sun Valley TTk Theme", "IconId", None),
+#     ],
+#     "Icon": [
+#         ("IconId", "assets/logo.ico"),
+#     ],
+# }
+import uuid
+
+upgradeid = (
+    "{" + str(uuid.uuid3(uuid.NAMESPACE_DNS, "UploaderGenius-App.UploaderGenius.org")).upper() + "}"
+)
+
 build_exe_options = {
-    "excludes": ["unittest"],
-    'includes': ["PIL",'moviepy','tsup',"requests",'UltraDict','jsons','lastversion','jsonschema','pystray','bcrypt','peewee','fastapi','pycountry','pyperclip'], # list of extra modules to include (from your virtualenv of system path),
-    'packages': [], # list of packages to include in the froze executable (from your application)
-    "zip_include_packages": ["encodings"],
+    "include_msvcr": True,
     "include_files": [
          ( './assets/', 'assets' ),
          ( './locales/', 'locales' ),
          ( './static/', 'static' )
 
-         ]
+         ],
+    'includes': ["PIL",'moviepy','tsup',"requests",'UltraDict','jsons','lastversion','jsonschema','pystray','bcrypt','peewee','fastapi','pycountry','pyperclip'], # list of extra modules to include (from your virtualenv of system path),
+
+
 }
 
-# base="Win32GUI" should be used only for Windows GUI app
-base = "Win32GUI" if sys.platform == "win32" else None
+bdist_rpm_options = {"icon": icon}
+
+bdist_msi_options = {
+    "add_to_path": False,
+    "install_icon": "assets/icon.ico",
+    "upgrade_code": upgradeid,
+    "target_name": "UploaderGenius",
+}
+bdist_mac_options = {"bundle_name": "UploaderGenius", "iconfile": "./assets/icon.ico"}
+
+bdist_dmg_options = {
+    "volume_label": "UploaderGenius",
+    "applications_shortcut": True,
+}
+
+version = "0.9.0"
 
 setup(
-    name="uploadergenius",
-    version="0.9",
-    description="Uploader Genius-your last social media publish management tool!",
-    options={"build_exe": build_exe_options},
-
-    executables = 
-        [
-            Executable(
-                "uploadergenius.py",
-                copyright="Copyright (C) 2023 TiktokaStudio",
-                base=base,
-                icon="assets\\icon.ico",
-                shortcut_name="uploadergenius",
-                # shortcut_dir="MyProgramMenu",
-            ),
-        ],
-
-
-
+    name="UploaderGenius",
+    version=version,
+    description="The only social media videos publish management tool you'll ever need",
+    executables=executables,
+    options={
+        "build_exe": build_exe_options,
+        "bdist_mac": bdist_mac_options,
+        "bdist_dmg": bdist_dmg_options,
+        "bdist_msi": bdist_msi_options,
+        "bdist_rpm": bdist_rpm_options,
+    },
 )
